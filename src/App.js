164f4bc449201789038a5b940da4db05e83214b5
Wendell <wendell@Wendells-Mac-mini.local>
2019-10-17 13:57:17 +0800

    add pages and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+// Components
+import NavBar from './components/NavBar';
+
+// Pages
+import Home from './pages/home';
+import SignUp from './pages/signup';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <Router>
+        <NavBar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
