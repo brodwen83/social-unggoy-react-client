43cf661787c6331b3aee0186577f07c974afb4b2
Wendell <wendell@Wendells-Mac-mini.local>
2019-10-17 14:34:08 +0800

    create custom theme using theme provider api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 // Components
 import NavBar from './components/NavBar';
 
@@ -10,9 +13,26 @@ import Home from './pages/home';
 import SignUp from './pages/signup';
 import Login from './pages/login';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff',
+    },
+  },
+});
+
 function App() {
   return (
-    <div className='App'>
+    <ThemeProvider theme={theme}>
       <Router>
         <NavBar />
         <div className='container'>
@@ -23,7 +43,7 @@ function App() {
           </Switch>
         </div>
       </Router>
-    </div>
+    </ThemeProvider>
   );
 }
 
