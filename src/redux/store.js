d390678067620000b3ec89ac7dda9e3e035e368e
Wendell <wendell@Wendells-Mac-mini.local>
2019-10-18 18:51:39 +0800

    create redux store

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import userReducer from './user/user.reducer';
+import dataReducer from './data/data.reducer';
+import uiReducer from './ui/ui.reducer';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const reducers = combineReducers({
+  user: userReducer,
+  data: dataReducer,
+  UI: uiReducer,
+});
+
+const store = createStore(
+  reducers,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
+export default store;
